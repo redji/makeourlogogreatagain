79011e693db72bfbf85883c2a7a69e1fa43a5c8f
Michał Furmaniak <michal.furmaniak@cdq.ch>
2019-10-23 09:35:06 +0200

    Initial layout

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,18 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+  <q-page class="flex">
+    <div class="game">
+      <div class="tasks">
+        <div class="pickup-cards"></div>
+        <div class="logo"></div>
+      </div>
+      <div class="goals">
+        <div class="stats">
+          <div class="time-spent">Time spent: 10s</div>
+          <div class="score">Score: 30%</div>
+        </div>
+        <div class="pickup-cards"></div>
+      </div>
+    </div>
   </q-page>
 </template>
 
